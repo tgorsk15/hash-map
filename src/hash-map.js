9b7b2e64f40da0961c5facc10dfeb93dcdc9c510
tgorsk15 <tgbusiness463@gmail.com>
2024-02-26 11:17:38 -0600

    Work on LinkedList functionality
    Each bucket will contain a linked list. This will allow multiple items to be stored
    into each bucket

--- src/hash-map.js
+++ src/hash-map.js
@@ -1,5 +1,15 @@
 /* eslint-disable import/prefer-default-export */
 
+// create a Node class for individual Linked Lists
+class Node {
+    constructor (key, value, nextNode = null) {
+        this.key = key;
+        this.value = value
+        this.nextNode = nextNode;
+    }
+}
+
+
 
 export const hashMap = function() {
     const maxBuckets = 16;
@@ -12,9 +22,9 @@ export const hashMap = function() {
         const primeNum = 7;
         for (let i = 0; i < key.length; i++) {
             hashCode = primeNum * hashCode + key.charCodeAt(i);
-            console.log(hashCode);
+            // console.log(hashCode);
             hashCode %= maxBuckets;
-            console.log(hashCode);
+            // console.log(hashCode);
         }
         // console.log(hashCode);
 
@@ -30,17 +40,47 @@ export const hashMap = function() {
             throw new Error('Index is out of bounds')
         }
 
+        // create a linkedList
+        let head = null;
+        let tail = null;
+        let length = 0;
 
         if (bucketStorage[index] === undefined ) {
             console.log(key);
 
-            const obj = {};
-            obj[key] = val;
-            bucketStorage[index] = obj;
+            const newNode = new Node(key, val)
+            console.log(newNode);
+            head = newNode;
+            length += 1;
+
+            // const obj = {};
+            // obj[key] = val;
+            bucketStorage[index] = head;
             console.log(bucketStorage);
+            console.log(length);
+            
         } else if (bucketStorage[index].hasOwnProperty(key)) {
             console.log('duplicate');
             // this conditional statement is not working currently
+
+        } else if (bucketStorage[index] !== undefined) {
+            head = bucketStorage[index];
+            const newNode = new Node(key, val);
+            let pointer = head;
+            console.log(pointer);
+
+            while (pointer.nextNode !== null) {
+                length += 1;
+                pointer = pointer.nextNode;
+                // console.log(pointer)
+            };
+            pointer.nextNode = newNode;
+            console.log(pointer)
+            tail = newNode;
+            console.log(tail);
+            length += 1;
+            console.log(length);
+
         }
         // console.log(bucketStorage)
         
--- src/index.js
+++ src/index.js
@@ -7,8 +7,13 @@ const testRun = hashMap();
 // testRun.createHash('Fred');
 
 testRun.addToMap('Fred', 'Smith');
-testRun.addToMap('Tully', 'McMuster');
 
+testRun.addToMap('Tully', 'McMuster');
+testRun.addToMap('Tully', 'Parker');
 testRun.addToMap('Tully', 'McMusterzzz');
+
+testRun.addToMap('Carlos', 'Zappas');
+
+// testRun.addToMap('Carla', 'Benzutto');
 
 
